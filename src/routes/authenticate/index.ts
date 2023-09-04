37edc1db51aeb4171e65d4c59ea63bfda6e15444
Satish K <satish.appanasamy@gmail.com>
2023-09-04 12:10:53 +0800

    refactor: login flow now uses DynamoDB to get user credentials

--- src/routes/authenticate/index.ts
+++ src/routes/authenticate/index.ts
@@ -3,8 +3,77 @@ import { login } from './authenticate';
 import {signup} from "./signup";
 
 const router = Router();
-
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     description: Generate JWT token
+ *     parameters:
+ *       - name: body
+ *         description: Username and password required for login
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: object
+ *           required:
+ *             - username
+ *             - password
+ *           properties:
+ *             username:
+ *               type: string
+ *               example: "johnDoe"
+ *             password:
+ *               type: string
+ *               format: password
+ *               example: "p@ssw0rd123"
+ *     responses:
+ *       200:
+ *         description: Returns JWT token
+ */
 router.post('/login', login);
+/**
+ * @swagger
+ * /auth/signup:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     description: Register a new user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         description: Username and password required for registration
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: object
+ *           required:
+ *             - username
+ *             - password
+ *           properties:
+ *             username:
+ *               type: string
+ *               example: "johnDoe"
+ *             password:
+ *               type: string
+ *               format: password
+ *               example: "p@ssw0rd123"
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: User registered successfully
+ *       400:
+ *         description: Username already exists
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/signup', signup);
 // router.post('/validate', validateTokenEndpoint);
 export default router;
---
+++ src/routes/authenticate/login.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import {SECRET_KEY, USERS_TABLE} from "../../constant/authSecrets";
+import getCommand from "../../utils/getCommand";
+
+export const login = async (req: Request, res: Response) => {
+	const { username, password } = req.body;
+	if (!username || !password) {
+		res.status(401).json({ message: 'Invalid credentials' });
+	}
+	
+	try {
+		// Fetch the user data from DynamoDB
+		const getUserParams = {
+			TableName: USERS_TABLE,
+			Key: { "username": username }
+		};
+		const result = await getCommand(getUserParams);
+		
+		const user = result.Item;
+		if (user) {
+			// Check if passwords match using bcrypt
+			const passwordIsValid = bcrypt.compareSync(password, user.hashedPassword);
+			
+			if (passwordIsValid) {
+				const token = jwt.sign({ username: user.username }, SECRET_KEY, { expiresIn: '1h' });
+				res.json({ token });
+			} else {
+				res.status(401).json({ message: 'Invalid credentials' });
+			}
+		} else {
+			res.status(404).json({ message: 'User not found' });
+		}
+	} catch (error) {
+		console.error("Error logging in:", error);
+		res.status(500).json({ message: 'Internal server error' });
+	}
+};
